39198d3ed09f296d26efbb5924580e2e898be181
Julian <j.sp@tuta.io>
2024-12-07 23:31:21 +0000

    Clean up

--- src/main/java/com/kijenasa/chess/ChessApplication.java
+++ src/main/java/com/kijenasa/chess/ChessApplication.java
@@ -1,14 +1,7 @@
 package com.kijenasa.chess;
 
-import com.kijenasa.chess.game.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-
 @SpringBootApplication
 public class  ChessApplication {
 
--- src/main/java/com/kijenasa/chess/game/GameController.java
+++ src/main/java/com/kijenasa/chess/game/GameController.java
@@ -1,6 +1,7 @@
 package com.kijenasa.chess.game;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,14 @@ public class GameController {
 
     @GetMapping
     public List<Game> getGame() {
-        Duration dur = Duration.ofDays(1);
         return List.of(new Game(
                 Duration.ofMinutes(15),
                 Instant.now()
         ));
+    }
+
+    @PostMapping
+    public void postMove() {
+
     }
 }
