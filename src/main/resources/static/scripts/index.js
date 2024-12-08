f885885fbdfc0a81b33394ebf23e9e709aa0f119
Julian <j.sp@tuta.io>
2024-12-08 04:32:37 +0000

    Create game button that sends user to game url

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -6,3 +6,23 @@ var boardConfig = {
 document.addEventListener("DOMContentLoaded", function() {
     var board1 = Chessboard('board1', boardConfig);
 });
+
+function sendPost(url, data, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 201) {
+            callback(xhr);
+        }
+    };
+
+    xhr.send(JSON.stringify(data));
+}
+
+function createGame() {
+    sendPost("http://localhost:8080/api/game", null, function(xhr) {
+        window.location.href = xhr.getResponseHeader("Location");
+    });
+}
